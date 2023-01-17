5dd6c4f903231a341e998ed7375ce15eefb6bd81
avinash <avinash.singh@theknowledgeacademy.com>
2023-01-17 13:04:01 +0530

    login page design fixed

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,6 +3,8 @@
 
 <head>
     <meta charset="UTF-8">
+    
+    <link href="https://fonts.googleapis.com/css2?family=Jost:wght@500&display=swap" rel="stylesheet">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="{{ asset('css/registerlogin.css') }}">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -12,29 +14,40 @@
 
 <body>
     <div class="main">
-        <input type="checkbox" id="chk" aria-hidden="true">
-        <div class="form-content">
-        {{-- signup --}}
-        {{ Form::open(['method' => 'POST', 'route' => 'register', 'class' => 'signup']) }}
-        <label for="chk" aria-hidden="true">Sign up</label>
-        {{ Form::text('name', null, ['placeholder' => 'User Name', 'required']) }}
-        {{ Form::email('email', null, ['placeholder' => 'E-mail', 'required']) }}
-        {{ Form::password('password', ['placeholder' => 'Password']) }}
-        {{ Form::password('password_confirmation', ['placeholder' => 'Password Confirm']) }}
-        <button id="register-button" type="submit"> {{ __('Register') }}</button>
-    {{ Form::close() }}
-    {{-- login  --}}
-    {{ Form::open(['method' => 'POST', 'route' => 'login', 'class' => 'login']) }}
-        <label for="chk" aria-hidden="true">Login</label>
-        {{ Form::email('email', null, ['placeholder' => 'E-mail', 'required']) }}
-        {{ Form::password('password', ['placeholder' => 'Password']) }}
-        <button id="login-button" type="submit">{{ __('Login') }}</button>
-        <a class="btn btn-link" href="{{ route('password.request') }}">
-            {{ __('Forgot Your Password?') }}
-        </a>
-    {{ Form::close() }}
-</div>
-        </div>
-</body>
+        <div class="form-content ">
+            {{-- signup --}}
+            {{ Form::open(['method' => 'POST', 'route' => 'register', 'class' => 'signup']) }}
+            <h2>Sign up</h2>
+            {{ Form::text('name', null, ['placeholder' => 'User Name', 'required']) }}
+            {{ Form::email('email', null, ['placeholder' => 'E-mail', 'required']) }}
+            {{ Form::password('password', ['placeholder' => 'Password']) }}
+            {{ Form::password('password_confirmation', ['placeholder' => 'Password Confirm']) }}
+            <button id="register-button" type="submit"> {{ __('Register') }}</button>
+            {{ Form::close() }}
 
+            {{-- login  --}}
+            {{ Form::open(['method' => 'POST', 'route' => 'login', 'class' => 'login']) }}
+            <h2>Login</h2>
+            {{ Form::email('email', null, ['placeholder' => 'E-mail', 'required']) }}
+            {{ Form::password('password', ['placeholder' => 'Password']) }}
+            <button id="login-button" type="submit">{{ __('Login') }}</button>
+            <a class="btn btn-link" href="{{ route('password.request') }}">
+                {{ __('Forgot Your Password?') }}
+            </a>
+            {{ Form::close() }}
+        </div>
+    </div>
+</body>
+<script src="{{ url('plugins/jquery/jquery.min.js') }}"></script>
+<script>
+
+        $('.login h2').on('click',function(){
+            $(this).parent().addClass('active');
+        });
+
+        $('.signup h2').on('click',function(){
+            $('.login').removeClass('active');
+        });
+
+</script>
 </html>
