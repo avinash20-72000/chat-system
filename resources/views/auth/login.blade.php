fea8b2b1fb622ccf803ad941bcb1673923a2a621
avinash <avinash.singh@theknowledgeacademy.com>
2023-01-19 12:00:35 +0530

    code optimize

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,12 +3,13 @@
 
 <head>
     <meta charset="UTF-8">
-    
+
     <link href="https://fonts.googleapis.com/css2?family=Jost:wght@500&display=swap" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" rel="stylesheet">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="{{ asset('css/registerlogin.css') }}">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ asset('plugins/toastr/toastr.min.css') }}">
     <meta name="token" content="{{ csrf_token() }}">
     <title>Chat | System</title>
 </head>
@@ -43,32 +44,49 @@
         <i class="fa fa-xmark"></i>
         <h2>Reset Password</h2>
         {{ Form::open(['method' => 'POST', 'route' => 'password.email']) }}
-        {{ Form::email('email', null , ['placeholder' => 'E-mail', 'required']) }}
+        {{ Form::email('email', null, ['placeholder' => 'E-mail', 'required']) }}
         <button id="reset-password-button" type="submit">{{ __('Reset') }}</button>
         {{ Form::close() }}
     </div>
 </body>
 <script src="{{ url('plugins/jquery/jquery.min.js') }}"></script>
+<script src="{{ asset('plugins/toastr/toastr.min.js') }}"></script>
 <script>
+    $(function() {
+        @if ($message = Session::get('success'))
+            toastr.success('{{ $message }}');
+        @endif
+        @if ($message = Session::get('failure'))
+            toastr.error('{{ $message }}');
+        @endif
+        @if ($message = Session::get('warning'))
+            toastr.warning('{{ $message }}');
+        @endif
+        @if ($errors->any())
+            @foreach ($errors->all() as $error)
+                toastr.warning('{{ $error }}');
+            @endforeach
+        @endif
 
-        $('.login h2').on('click',function(){
-            $(this).parent().addClass('active');
-        });
+    });
 
-        $('.signup h2').on('click',function(){
-            $('.login').removeClass('active');
-        });
+    $('.login h2').on('click', function() {
+        $(this).parent().addClass('active');
+    });
 
-        function openModal()
-        {
-            $('.main').hide();
-            $('.forget-password').show();
-        }
+    $('.signup h2').on('click', function() {
+        $('.login').removeClass('active');
+    });
 
-        $('.forget-password i').on('click',function(){
-            $('.forget-password').hide();
-            $('.main').show();
-        });
+    function openModal() {
+        $('.main').hide();
+        $('.forget-password').show();
+    }
 
+    $('.forget-password i').on('click', function() {
+        $('.forget-password').hide();
+        $('.main').show();
+    });
 </script>
+
 </html>
